4d8756c9758f6a3a761cfc07d44f2e7e2111b760
Manuel Macaraig <mdmacaraig@up.edu.ph>
2024-07-12 09:26:03 +0800

    add register page, flowbite plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+const flowbitePlugin = require('flowbite/plugin');
 const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+
   theme: {
     extend: {
+      colors: {
+        primary: {
+          50: '#ecfdf5',
+          100: '#d1fae5',
+          200: '#a7f3d0',
+          300: '#6ee7b7',
+          400: '#34d399',
+          500: '#10b981',
+          600: '#059669',
+          700: '#047857',
+          800: '#065f46',
+          900: '#064e3b'
+        }
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    require('@tailwindcss/forms'), flowbitePlugin
   ],
 }
 
