81d2d7a5daaa702bd4b8bd8c6d281af6a1cd52c1
Cedric Legara <legara.cedric@gmail.com>
2024-07-12 11:20:06 +0800

    fix: new post via form standard

--- src/routes/users/+page.server.js
+++ src/routes/users/+page.server.js
@@ -1,5 +1,4 @@
 import { createUserDB, getUsersDB } from '$lib/db';
-import { redirect } from '@sveltejs/kit';
 
 export const actions = {
     register: async ({cookies, request}) => {
@@ -7,9 +6,9 @@ export const actions = {
             const data = await request.formData();
             const args = [...data.values()];
             const newUser = await createUserDB(...args.map((val) => val === '' ? null : val));
-        } catch (error) {
+            return { user: newUser, status: 201}
+          } catch (error) {
             return { status: 503 }
         }
-        throw redirect(303, '/dashboard'); // idk how to make this work
     }
 }
