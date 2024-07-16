42360765b04d423e47b3ce96087e808954426192
Manuel Macaraig <mdmacaraig@up.edu.ph>
2024-07-16 08:22:12 +0800

    fix sidebar

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,73 @@
+<script>
+    import { Card, Button, Drawer, CloseButton } from 'flowbite-svelte';
+    import { ArrowRightOutline, InfoCircleSolid, HomeSolid } from 'flowbite-svelte-icons';
+    import { sineIn } from 'svelte/easing';
+    import Sidebar from './sidebar.svelte';
+  let hidden1 = true;
+  let transitionParams = {
+    x: -320,
+    duration: 200,
+    easing: sineIn
+  };
+    function handleClick() {
+        window.location.href = "/";
+    }
+</script>
+<aside class="hidden lg:block fixed z-10 top-0 pb-3 px-6 w-full justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
+    <Sidebar></Sidebar>
+</aside>
+
+<Drawer transitionType="fly" {transitionParams} bind:hidden={hidden1} id="sidebar1" class="justify-between">
+    <Sidebar></Sidebar>
+</Drawer>
+
+<!--navbar-->
+<div class="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
+  <div class="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
+      <div class="px-6 flex items-center justify-between space-x-4 2xl:container">
+          <h5 hidden class="text-2xl text-gray-600 font-medium lg:block">Dashboard</h5>
+          <!--hamburger-->
+          <button on:click={() => (hidden1 = false)} class="w-12 h-16 -mr-2 border-r lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 my-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+          </button>
+          <div class="flex space-x-4">
+              <!--search bar -->
+              <div hidden class="md:block">
+                  <div class="relative flex items-center text-gray-400 focus-within:text-cyan-400">
+                      <span class="absolute left-4 h-6 flex items-center pr-3 border-r border-gray-300">
+                      <svg xmlns="http://ww50w3.org/2000/svg" class="w-4 fill-current" viewBox="0 0 35.997 36.004">
+                          <path id="Icon_awesome-search" data-name="search" d="M35.508,31.127l-7.01-7.01a1.686,1.686,0,0,0-1.2-.492H26.156a14.618,14.618,0,1,0-2.531,2.531V27.3a1.686,1.686,0,0,0,.492,1.2l7.01,7.01a1.681,1.681,0,0,0,2.384,0l1.99-1.99a1.7,1.7,0,0,0,.007-2.391Zm-20.883-7.5a9,9,0,1,1,9-9A8.995,8.995,0,0,1,14.625,23.625Z"></path>
+                      </svg>
+                      </span>
+                      <input type="search" name="leadingIcon" id="leadingIcon" placeholder="Search here" class="w-full pl-14 pr-4 py-2.5 rounded-xl text-sm text-gray-600 outline-none border border-gray-300 focus:border-cyan-300 transition">
+                  </div>
+              </div>
+              <!--/search bar -->
+              <button aria-label="search" class="w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200 md:hidden">
+                  <svg xmlns="http://ww50w3.org/2000/svg" class="w-4 mx-auto fill-current text-gray-600" viewBox="0 0 35.997 36.004">
+                      <path id="Icon_awesome-search" data-name="search" d="M35.508,31.127l-7.01-7.01a1.686,1.686,0,0,0-1.2-.492H26.156a14.618,14.618,0,1,0-2.531,2.531V27.3a1.686,1.686,0,0,0,.492,1.2l7.01,7.01a1.681,1.681,0,0,0,2.384,0l1.99-1.99a1.7,1.7,0,0,0,.007-2.391Zm-20.883-7.5a9,9,0,1,1,9-9A8.995,8.995,0,0,1,14.625,23.625Z"></path>
+                  </svg>
+              </button>
+              <button aria-label="chat" class="w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-auto text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                  </svg>
+              </button>
+              <button aria-label="notification" class="w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 m-auto text-gray-600" viewBox="0 0 20 20" fill="currentColor">
+                      <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+                  </svg>
+              </button>
+          </div>
+      </div>
+  </div>
+  <!--actual dashboard-->
+  
+
+  <slot></slot>
+
+  
+
+</div>
---
+++ src/routes/register/teacher/+page.svelte
@@ -0,0 +1,178 @@
+<script>
+    import { goto, onNavigate } from "$app/navigation";
+    import { browser } from "$app/environment";
+    import { Input, Label, Button, Select, Alert } from "flowbite-svelte";
+    import { ArrowLeftOutline } from "flowbite-svelte-icons";
+    let selected = "";
+    let password, confirm_password;
+    let match = true;
+    
+    let usertypes = [
+        {
+            value: "dflcd",
+            name: "Department of Family Life and Child Development",
+        },
+        {
+            value: "dctid",
+            name: "Department of Clothing, Textiles, and Interior Design",
+        },
+        { value: "dfsn", name: "Department of Food Science and Nutrition" },
+    ];
+    async function handleSubmit(){
+        if(!password  || password != confirm_password) {
+            match = false;
+            return
+        }
+        goto(`/dashboard`)
+    }
+    function navBack() {
+        if (browser) window.history.back();
+    }
+</script>
+
+<section class="bg-gray-200 min-h-screen flex items-center justify-center p-5">
+    <div class="bg-gray-100 w-full flex rounded-xl max-w-4xl drop-shadow-xl">
+        <div class="w-full ps-3 pe-3 pb-6">
+            <div
+                class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 w-full"
+            >
+                <div class="sm:w-full sm:mx-auto w-full relative">
+                    <Button
+                        color="alternative"
+                        class="!p-2 absolute top-0 left-0"
+                        on:click={() => {
+                            navBack();
+                        }}><ArrowLeftOutline class="w-6 h-6" /></Button
+                    >
+                    <img
+                        class="mx-auto h-10 w-auto"
+                        src="https://che.upd.edu.ph/wp-content/uploads/2022/10/DFLCD-Logo.png"
+                        alt="DFLCD"
+                    />
+                    <h2
+                        class="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+                    >
+                        Make a new account
+                    </h2>
+                </div>
+
+                <div class="mt-5">
+                    {#if !match}
+                    <Alert border color="red">
+                        <span class="font-medium">Passwords don't match!</span>
+                        Please try again.
+                      </Alert>
+                    {/if}
+                    <form on:submit|preventDefault={handleSubmit} class="space-y-3" method="POST">
+                        <div class="flex flex-wrap align-center">
+                            <div
+                                class="sm:w-1/2 w-full mt-3 space-y-4 sm:pr-1.5"
+                            >
+                                <div class="flex items-center space-x-2">
+                                    <!--names-->
+                                    <div class="w-1/2">
+                                        <Label for="first_name" class="mb-2"
+                                            >First name</Label
+                                        >
+                                        <Input
+                                            type="text"
+                                            id="first_name"
+                                            placeholder="Juan"
+                                            required
+                                        />
+                                    </div>
+                                    <div class="w-1/2">
+                                        <Label for="last_name" class="mb-2"
+                                            >Last name</Label
+                                        >
+                                        <Input
+                                            type="text"
+                                            id="last_name"
+                                            placeholder="Dela Cruz"
+                                            required
+                                        />
+                                    </div>
+                                </div>
+                                <div>
+                                    <Label for="phone" class="mb-2"
+                                        >Phone number</Label
+                                    >
+                                    <Input
+                                        type="tel"
+                                        id="phone"
+                                        placeholder="09xxxxxxxxx"
+                                        pattern={"[0-9]{11}"}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <Label>
+                                        Position
+                                        <Select
+                                            class="mt-2"
+                                            items={usertypes}
+                                            bind:value={selected}
+                                            required
+                                        />
+                                    </Label>
+                                </div>
+                            </div>
+                            <div
+                                class="sm:w-1/2 w-full mt-3 space-y-4 sm:pl-1.5"
+                            >
+                                <!--names-->
+                                
+                                <div>
+                                    <Label for="email" class="mb-2"
+                                        >Email address</Label
+                                    >
+                                    <Input
+                                        type="email"
+                                        id="email"
+                                        autocomplete="email"
+                                        placeholder="myemail@email.ph"
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <Label for="password" class="mb-2"
+                                        >Password</Label
+                                    >
+                                    <Input
+                                        bind:value={password}
+                                        type="password"
+                                        id="password"
+                                        placeholder="•••••••••"
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <Label for="confirm_password" class="mb-2">Confirm password</Label>
+                                    <Input bind:value={confirm_password} type="password" id="confirm_password" placeholder="•••••••••" required />
+                                  </div>
+                            </div>
+                        </div>
+                            <Button on:click={() =>{handleSubmit()}} type="submit" class="w-full">Submit</Button>
+                    </form>
+
+                    <p class="mt-5 text-center text-sm text-gray-500">
+                        Already have an account?
+                        <a
+                            href="/"
+                            class="font-semibold leading-6 text-emerald-600 hover:text-emerald-500"
+                            >Sign in</a
+                        >
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme("colors.gray.200");
+    }
+</style>
