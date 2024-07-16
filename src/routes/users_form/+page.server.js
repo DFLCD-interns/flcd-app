90cfdea722a18a0d8801f3b822d85efa945e99c4
Cedric Legara <legara.cedric@gmail.com>
2024-07-16 12:37:47 +0800

    feat: backbone forms for create class, batches, and fixed DB insertion function to be generalized

--- src/routes/users_form/+page.server.js
+++ src/routes/users_form/+page.server.js
@@ -1,13 +1,12 @@
-import { createUserDB, getUsersDB } from '$lib/db';
+import { insertIntoTableDB } from '$lib/db';
 import { redirect } from '@sveltejs/kit';
 
 export const actions = {
     register: async ({cookies, request}) => {
         try {
             const data = await request.formData();
-            const args = [...data.values()];
-            const newUser = await createUserDB(...args.map((val) => val === '' ? null : val));
-        } catch (error) {
+            const newUser = await insertIntoTableDB("users", data);
+        } catch (error) {   
             return { status: 503 }
         }
         throw redirect(303, '/dashboard'); // idk how to make this work
