5d68f1d4b7d292b31b07e700d433e7bba7dc801c
Manuel Macaraig <mdmacaraig@up.edu.ph>
2024-07-17 17:00:37 +0800

    update request form

--- src/routes/dashboard/requestsform/+page.svelte
+++ src/routes/dashboard/requestsform/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import { browser } from "$app/environment";
-    import { Button, Card, GradientButton, Input, Label, MultiSelect, Select, Textarea, } from "flowbite-svelte";
-    import { AddressBookOutline, ArrowLeftOutline, ChevronLeftOutline, UserAddSolid, } from "flowbite-svelte-icons";
+    import { Button, Card, GradientButton, Input, Label, MultiSelect, Select, Textarea, Tabs, TabItem, } from "flowbite-svelte";
+    import { AddressBookOutline, ArrowLeftOutline, BuildingSolid, ChevronLeftOutline, ComputerSpeakerSolid, UserAddSolid, } from "flowbite-svelte-icons";
     function navBack() {
         if (browser) window.history.back();
     }
@@ -46,13 +46,13 @@
 
 <div class="px-10 py-10" use:onload>
     <!--header-->
-    <div class="flex items-center gap-5 mb-5">
+    <div class="flex items-center gap-4 mb-5">
         <Button
             on:click={() => {
                 navBack();
             }}
             color="alternative"
-            class="w-auto"><ArrowLeftOutline size="md" /></Button
+            class="w-auto p-0"><ChevronLeftOutline size="xl" /></Button
         >
         <h5
             class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
@@ -60,110 +60,215 @@
             Request Form
         </h5>
     </div>
-
-    <!--actual form-->
-    <Card class="max-w-full">
-        <form  class="flex flex-col space-y-6" action="/">
-            <h3 class="text-xl font-medium text-gray-900 dark:text-white">
-                Borrowing Equipment
-            </h3>
-            <hr />
-            <Label class="space-y-2">
-                <span>Equipment</span>
-                <MultiSelect
-                    class="mt-2"
-                    items={equipment}
-                    bind:value={selectedEq}
-                    required
-                />
-            </Label>
-            <div class="grid gap-6 mb-6 md:grid-cols-4">
-                {#each selectedEq as eq}
-                    <Label>
-                        <span
-                            >Qty of {equipment.find((x) => x.value == eq)
-                                .name}</span
-                        >
-                        <Input type="number" name="qty" required />
-                    </Label>
-                {/each}
-            </div>
-            <hr />
-            <div class="grid gap-6 mb-6 md:grid-cols-2">
-                <Label class="space-y-2">
-                    <span>Use Date</span>
-                    <Input type="date" name="dateneeded" required />
-                </Label>
-                <Label class="space-y-2">
-                    <span>Use Time</span>
-                    <Input type="time" name="timmeneeded" required />
-                </Label>
-                <Label class="space-y-2">
-                    <span>Return Date</span>
-                    <Input type="date" name="dateneeded" required />
-                </Label>
-                <Label class="space-y-2">
-                    <span>Return Time</span>
-                    <Input type="time" name="timmeneeded" required />
-                </Label>
-                <Label class="space-y-2">
-                    <span>Venue</span>
-                    <Input type="text" name="venue" required />
-                </Label>
-                <Label class="space-y-2">
-                    <span>Purpose</span>
-                    <Textarea rows=1 type="text" name="venue" required />
-                </Label>
-                <Label class="space-y-2">
-                    <span>Name of Coordinating Faculty/Admin</span>
-                    <Input type="name" name="name" required />
-                </Label>
-                <Label class="space-y-2">
-                    <span>Email of Coordinating Faculty/Admin</span>
-                    <Input type="email" name="email" required />
-                </Label>
-            </div>
-            <hr />
-            <div class="grid gap-6 mb-6 md:grid-cols-2">
-                <Label>
-                    <span>Department</span>
-                    <Select
-                        class="mt-2"
-                        items={depts}
-                        bind:value={selectedDept}
-                    />
-                </Label>
-                {#key isOther}
+    <Tabs tabStyle="pill">
+        <TabItem open>
+            <span slot="title" class="flex gap-2"><ComputerSpeakerSolid/>Equipment</span>
+            <Card class="max-w-full">
+                <form class="flex flex-col space-y-6" action="/">
+                    <h3
+                        class="text-xl font-medium text-gray-900 dark:text-white"
+                    >
+                        Borrowing Equipment
+                    </h3>
+                    <hr />
                     <Label class="space-y-2">
-                        <span>(Non-CHE) Department</span>
-                        <Input
-                            disabled={!isOther}
-                            type="text"
-                            name="department"
+                        <span>Equipment</span>
+                        <MultiSelect
+                        tabindex=0
+                            class="mt-2"
+                            items={equipment}
+                            bind:value={selectedEq}
+                            required
                         />
                     </Label>
-                {/key}
-                <Label class="space-y-2">
-                    <span>Course</span>
-                    <Input
-                        type="text"
-                        name="course"
-                        required={isStudent}
-                        disabled={!isStudent}
-                    />
-                </Label>
-                <Label class="space-y-2">
-                    <span>Preschool Section Assigned</span>
-                    <Input
-                        type="text"
-                        name="pschool_section"
-                        required={isStudent}
-                        disabled={!isStudent}
-                    />
-                </Label>
-               
-            </div>
-            <GradientButton shadow color="green"> Request </GradientButton>
-        </form>
-    </Card>
+                    <div class="grid gap-6 mb-6 md:grid-cols-4">
+                        {#each selectedEq as eq}
+                            <Label>
+                                <span
+                                    >Quantity of {equipment.find(
+                                        (x) => x.value == eq,
+                                    ).name}</span
+                                >
+                                <Input type="number" name={eq} autocomplete={`1`} required />
+                            </Label>
+                        {/each}
+                    </div>
+                    <hr />
+                    <div class="grid gap-6 mb-6 md:grid-cols-2">
+                        <Label class="space-y-2">
+                            <span>Use Date</span>
+                            <Input type="date" name="dateneeded" required />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Use Time</span>
+                            <Input type="time" name="timmeneeded" required />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Return Date</span>
+                            <Input type="date" name="dateneeded" required />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Return Time</span>
+                            <Input type="time" name="timmeneeded" required />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Venue</span>
+                            <Input type="text" name="venue" required />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Purpose</span>
+                            <Textarea
+                                rows="1"
+                                type="text"
+                                name="venue"
+                                required
+                            />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Name of Coordinating Faculty/Admin</span>
+                            <Input type="name" name="name" required />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Email of Coordinating Faculty/Admin</span>
+                            <Input type="email" name="email" required />
+                        </Label>
+                    </div>
+                    <hr />
+                    <div class="grid gap-6 mb-6 md:grid-cols-2">
+                        <Label>
+                            <span>Department</span>
+                            <Select
+                                class="mt-2"
+                                items={depts}
+                                bind:value={selectedDept}
+                            />
+                        </Label>
+                        {#key isOther}
+                            <Label class="space-y-2">
+                                <span>(Non-CHE) Department</span>
+                                <Input
+                                    disabled={!isOther}
+                                    type="text"
+                                    name="department"
+                                />
+                            </Label>
+                        {/key}
+                        <Label class="space-y-2">
+                            <span>Course</span>
+                            <Input
+                                type="text"
+                                name="course"
+                                required={isStudent}
+                                disabled={!isStudent}
+                            />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Preschool Section Assigned</span>
+                            <Input
+                                type="text"
+                                name="pschool_section"
+                                required={isStudent}
+                                disabled={!isStudent}
+                            />
+                        </Label>
+                    </div>
+                    <GradientButton shadow color="green"  type="submit">
+                        Request
+                    </GradientButton>
+                </form>
+            </Card>
+        </TabItem>
+        <TabItem>
+            <span slot="title" class="flex gap-2"><BuildingSolid/>Venue</span>
+            <Card class="max-w-full">
+                <form class="flex flex-col space-y-6" action="/">
+                    <h3
+                        class="text-xl font-medium text-gray-900 dark:text-white"
+                    >
+                        Reserve a Venue
+                    </h3>
+                    <hr />
+                    <div>
+                        <Label class="space-y-2">
+                            <span>Room</span>
+                            <Input type="text" name="room" required />
+                        </Label>
+                    </div>
+                    
+                    <hr />
+                    <div class="grid gap-6 mb-6 md:grid-cols-2">
+                        <Label class="space-y-2">
+                            <span>Activity</span>
+                            <Input type="text" name="activity" required />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Use Date</span>
+                            <Input type="date" name="dateneeded" required />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Acitivity Start Time</span>
+                            <Input type="time" name="start_time" required />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Acitivity End Time</span>
+                            <Input type="time" name="end_time" required />
+                        </Label>
+                        
+                        <Label class="space-y-2">
+                            <span>Name of Coordinating Faculty/Admin</span>
+                            <Input type="name" name="name" required />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Email of Coordinating Faculty/Admin</span>
+                            <Input type="email" name="email" required />
+                        </Label>
+                    </div>
+                    <hr />
+                    <div class="grid gap-6 mb-6 md:grid-cols-2">
+                        <Label>
+                            <span>Department</span>
+                            <Select
+                                class="mt-2"
+                                items={depts}
+                                bind:value={selectedDept}
+                            />
+                        </Label>
+                        {#key isOther}
+                            <Label class="space-y-2">
+                                <span>(Non-CHE) Department</span>
+                                <Input
+                                    disabled={!isOther}
+                                    type="text"
+                                    name="department"
+                                />
+                            </Label>
+                        {/key}
+                        <Label class="space-y-2">
+                            <span>Course</span>
+                            <Input
+                                type="text"
+                                name="course"
+                                required={isStudent}
+                                disabled={!isStudent}
+                            />
+                        </Label>
+                        <Label class="space-y-2">
+                            <span>Preschool Section Assigned</span>
+                            <Input
+                                type="text"
+                                name="pschool_section"
+                                required={isStudent}
+                                disabled={!isStudent}
+                            />
+                        </Label>
+                    </div>
+                    <GradientButton shadow color="green" type="submit">
+                        Request
+                    </GradientButton>
+                </form>
+            </Card>
+        </TabItem>
+    </Tabs>
+
+    <!--actual form-->
 </div>
