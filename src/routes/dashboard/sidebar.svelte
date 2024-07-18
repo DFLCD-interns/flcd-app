3a0d0afcd0e40599d36787d7561ddb9b40f0f477
Clarisse Bianca Bucu <isaybucu@gmail.com>
2024-07-19 07:56:26 +0800

    placeholder logout

--- src/routes/dashboard/sidebar.svelte
+++ src/routes/dashboard/sidebar.svelte
@@ -55,6 +55,10 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
         </svg>
-        <span class="group-hover:text-gray-700">Logout</span>
+        <a
+            href="/"
+            class="group-hover:text-gray-700"
+            >Logout</a
+        >
     </button>
 </div>
--- src/routes/register/teacher/+page.server.js
+++ src/routes/register/teacher/+page.server.js
@@ -1,14 +1,14 @@
-import { createUserDB, getUsersDB } from '$lib/db';
+import { insertIntoTableDB } from '$lib/db';
 import { redirect } from '@sveltejs/kit';
 
 export const actions = {
     register: async ({cookies, request}) => {
         try {
             const data = await request.formData();
-            const args = [...data.values()];
-            const response = await createUserDB(...args.map((val) => val === '' ? null : val));
+            const response = await insertIntoTableDB("users", data);
             return response.ok;
-        } catch (error) {
+            
+        } catch (error) {   
             console.error("Action failed:", error.message);
             return response.ok;
         }
