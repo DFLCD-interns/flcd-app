b43bc4ced7a3140b8104db2600bdeb16243c8587
Manuel Macaraig <mdmacaraig@up.edu.ph>
2024-08-01 23:30:42 +0800

    dashboard fix

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -5,9 +5,6 @@
     import AdminView from "./admin-view.svelte";
     import NonAdminView from "./non-admin-view.svelte";
     let isAdmin = false;
-    function handleClick() {
-        window.location.href = "/";
-    }
     console.log(data.current_user.user_id)
     console.log(`request: ${data.equipment_requests.length}`)
 </script>
