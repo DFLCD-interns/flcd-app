a6ad8ea5ef8f8a487e9635b0c721e6ea81b3c2cc
jnnln <janinalaine.platero@gmail.com>
2024-11-14 14:20:26 +0800

    edit/delete users

--- src/routes/dashboard/databases/users/+page.server.js
+++ src/routes/dashboard/databases/users/+page.server.js
@@ -1,9 +1,104 @@
-import * as db from '$lib/server/db.js';
+// import * as db from '$lib/server/db.js';
 
-/** @type {import('./$types').PageServerLoad} */
-export async function load() {
-	return {
-		user: await db.getUsersListDB(),
-		userTypes: await db.getUserTypesDB(),
-	};
+// /** @type {import('./$types').PageServerLoad} */
+// export async function load() {
+// 	return {
+// 		user: await db.getUsersListDB(),
+// 		userTypes: await db.getUserTypesDB(),
+// 	};
+// }
+
+import { getUsersListDB, deleteFromTableDB, updateTableDB } from '$lib/server/db';
+import { fail } from "@sveltejs/kit";
+/** @type {import('./$types').Actions} */
+
+export const actions = {
+	deleteUser: async ({ request }) => {
+        const formData = await request.formData();
+        const id = formData.get("id");
+        const fd = { id };
+        const form_data = new FormData();
+        for (let key in fd) {
+            form_data.append(key, fd[key]);
+        }
+        
+        try {
+            await deleteFromTableDB('users', form_data);
+        } catch (error) {   
+            console.error("Error: ", error.message);
+            return fail(500, {
+                message: error.message,
+            }); 
+        }
+    },
+
+    editUser: async ({ request }) => {
+        const formData = await request.formData();
+        const id = formData.get("id");
+        const first_name = formData.get("first_name"); const old_first_name = formData.get("old_first_name");
+        const last_name = formData.get("last_name"); const old_last_name = formData.get("old_last_name");
+        const email = formData.get("email"); const old_email = formData.get("old_email");
+        const phone = formData.get("phone"); const old_phone = formData.get("old_phone");
+        const student_number = formData.get("student_number"); const old_student_number = formData.get("old_student_number");
+		const course = formData.get("course"); const old_course = formData.get("old_course");
+		const department = formData.get("department"); const old_department = formData.get("old_department");
+		const access_level = formData.get("access_level"); const old_access_level = formData.get("old_access_level");
+
+        if (first_name===old_first_name && last_name===old_last_name && email===old_email && phone===old_phone && student_number==old_student_number && course==old_course && department==old_department && access_level==old_access_level) {
+            return fail(500, {
+                message: "No changes made.",
+            });
+        }
+
+		let users = await getUsersListDB();
+
+        let userStudentNumbers = users.map((item) => (item.student_number));
+        if (student_number !== old_student_number) {
+            if (userStudentNumbers.includes(student_number)) {
+                return fail(500, {
+                    message: "This student number already exists!",
+                });
+            }
+        }
+
+        let userEmails = users.map((item) => (item.email));
+		console.log(old_email)
+        if (email !== old_email) {
+            if (userEmails.includes(email)) {
+                return fail(500, {
+                    message: "This email already exists!",
+                });
+            }
+        }
+
+        const id_fd = { id };
+        const id_data = new FormData();
+        for (let key in id_fd) {
+            id_data.append(key, id_fd[key]);
+        }
+
+        const fd = {
+            first_name,
+			last_name,
+			email,
+			phone,
+			student_number,
+			course,
+			department,
+			access_level
+        };
+        const form_data = new FormData();
+        for (let key in fd) {
+            form_data.append(key, fd[key]);
+        }
+        
+        try {
+            await updateTableDB('users', id_data, form_data);
+        } catch (error) {   
+            console.error("Error: ", error.message);
+            return fail(500, {
+                message: error.message,
+            }); 
+        }
+    },
 }
--- src/routes/dashboard/databases/users/+page.svelte
+++ src/routes/dashboard/databases/users/+page.svelte
@@ -3,7 +3,9 @@
 	export let data;
 
   import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Search, Button, Input, Modal, Label, GradientButton, MultiSelect } from 'flowbite-svelte';
-  import { EditOutline, TrashBinOutline, FilterSolid, ChevronSortOutline } from 'flowbite-svelte-icons';
+  import { CheckOutline, CloseOutline, EditOutline, TrashBinOutline, FilterSolid, ChevronSortOutline } from 'flowbite-svelte-icons';
+  import { enhance } from '$app/forms';
+  import toast from 'svelte-french-toast';
 
   let users = data.user;
   let access_level = data.current_user.access_level;
@@ -19,6 +21,8 @@
   let EditModal=false;
 
   let editUser;
+  let editingRowIndex = -1;
+  let eq;
 
   
 
@@ -95,6 +99,57 @@
       sortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
     }
 
+    function editRow(row) {
+      editUser = {...row};
+      editingRowIndex = row.id;
+    }
+
+    /*-- toast logic --*/
+    let loading = false
+
+    const deleteUser = () => {
+        loading = true;
+        return async ({ result, update }) => {
+            switch (result.type) {
+                case 'success':
+                    DeleteModal = false;
+                    toast.success("User deleted successfully!");
+                    await update();
+                    break;
+                case 'failure':
+                    const errorMessage = result.data.message || 'Failed to delete user.';
+                    toast.error(errorMessage);
+                    break;
+                default:
+                    await update();
+            }
+            loading = false;
+        }
+    }
+    const saveChanges = () => {
+        loading = true;
+        return async ({ result, update }) => {
+            switch (result.type) {
+                case 'success':
+                    editingRowIndex = -1;
+                    toast.success("Changes saved successfully!");
+                    await update();
+                    break;
+                case 'failure':
+                    const errorMessage = result.data.message || 'Failed to save changes.';
+                    if (errorMessage === "No changes made.") {
+                      editingRowIndex = -1;
+                    } else {
+                      toast.error(errorMessage);
+                    }
+                    break;
+                default:
+                    await update();
+            }
+            loading = false;
+        }
+    }
+
 </script>
   
 <div class="p-10">
@@ -116,10 +171,6 @@
   {#if users != null }
   <Table shadow>
     <TableHead>
-      {#if access_level !=4}
-      <TableHeadCell></TableHeadCell>
-      <TableHeadCell></TableHeadCell>
-      {/if}
       {#each tableHead as head}
       {#if head != 'dateString'}
       <TableHeadCell>
@@ -130,29 +181,103 @@
       </TableHeadCell>
       {/if}
       {/each}
+    <TableHeadCell>
+    </TableHeadCell>
     </TableHead>
     <TableBody tableBodyClass="divide-y">
       {#each users as user}
       <TableBodyRow>
-        {#if access_level !=4}
         <TableBodyCell>
-          <button on:click={() => {EditModal = true; editUser = user}}><EditOutline  class="text-green-600"/></button>
+          {#if editingRowIndex === user.id}
+              <Input type="text" bind:value={editUser.id} />  
+            {:else}
+            {user.id}
+          {/if}
+          </TableBodyCell>
+        <TableBodyCell>
+          {#if editingRowIndex === user.id}
+            <Input type="text" bind:value={editUser.first_name} />
+          {:else}
+            {user.first_name}
+          {/if}
+          </TableBodyCell>
+        <TableBodyCell>
+          {#if editingRowIndex === user.id}
+            <Input type="text" bind:value={editUser.last_name} />
+          {:else}
+            {user.last_name}
+          {/if}
         </TableBodyCell>
         <TableBodyCell>
-          <button on:click={() => {DeleteModal = true; userName = user.first_name+" "+user.last_name}}><TrashBinOutline class="text-green-600"/></button>
+          {#if editingRowIndex === user.id}
+            <Input type="text" bind:value={editUser.email} />
+          {:else}
+            {user.email}
+          {/if}
         </TableBodyCell>
-        {/if}
-        <TableBodyCell>{user.id}</TableBodyCell>
-        <TableBodyCell>{user.first_name}</TableBodyCell>
-        <TableBodyCell>{user.last_name}</TableBodyCell>
-        <TableBodyCell>{user.email}</TableBodyCell>
-        <TableBodyCell>{user.phone}</TableBodyCell>
-        <TableBodyCell>{user.student_number}</TableBodyCell>
-        <TableBodyCell>{user.course}</TableBodyCell>
-        <TableBodyCell>{user.department}</TableBodyCell>
-        <TableBodyCell><Input type="number" min=1 max=5 value={user?.access_level}></Input></TableBodyCell>
-        <TableBodyCell>{user.description}</TableBodyCell>
-        <TableBodyCell>{formatDate(user.created)}</TableBodyCell>
+        <TableBodyCell>
+          {#if editingRowIndex === user.id}
+            <Input type="text" bind:value={editUser.phone} />
+          {:else}
+            {user.phone}
+          {/if}
+        </TableBodyCell>
+        <TableBodyCell>
+          {#if editingRowIndex === user.id}
+            <Input type="text" bind:value={editUser.student_number} />
+          {:else}
+            {user.student_number}
+          {/if}
+        </TableBodyCell>
+        <TableBodyCell>
+          {#if editingRowIndex === user.id}
+            <Input type="text" bind:value={editUser.course} />
+          {:else}
+            {user.course}
+          {/if}
+        </TableBodyCell>
+        <TableBodyCell>
+          {#if editingRowIndex === user.id}
+            <Input type="text" bind:value={editUser.department} />
+          {:else}
+            {user.department}
+          {/if}
+        </TableBodyCell>
+        <TableBodyCell>
+          {#if editingRowIndex === user.id}
+            <Input type="text" bind:value={editUser.access_level} />
+          {:else}
+            {user?.access_level}
+          {/if}
+        </TableBodyCell>
+        <TableBodyCell>
+          {user.description}
+        </TableBodyCell>
+        <TableBodyCell>
+          {formatDate(user.created)}
+        </TableBodyCell>
+
+        <TableBodyCell>
+          {#if editingRowIndex === user.id}
+            <form method="POST" action="?/editUser" use:enhance={saveChanges}>
+              <input type="hidden" name="id" value={editUser.id}/>
+              <input type="hidden" name="first_name" value={editUser.first_name}/> <input type="hidden" name="old_first_name" value={user.first_name}/>
+              <input type="hidden" name="last_name" value={editUser.last_name}/> <input type="hidden" name="old_last_name" value={user.last_name}/>
+              <input type="hidden" name="email" value={editUser.email}/> <input type="hidden" name="old_email" value={user.email}/>
+              <input type="hidden" name="phone" value={editUser.phone}/> <input type="hidden" name="old_phone" value={user.phone}/>
+              <input type="hidden" name="student_number" value={editUser.student_number}/> <input type="hidden" name="old_student_number" value={user.student_number}/>
+              <input type="hidden" name="course" value={editUser.course}/> <input type="hidden" name="old_course" value={user.course}/>
+              <input type="hidden" name="department" value={editUser.department}/> <input type="hidden" name="old_department" value={user.department}/>
+              <input type="hidden" name="access_level" value={editUser.access_level}/> <input type="hidden" name="old_access_level" value={user.access_level}/>
+              <button type="submit" disabled={loading} class="mr-2"><CheckOutline class="text-green-600 mr-2"/></button>
+              <button on:click={() => {editingRowIndex = -1}} disabled={loading}><CloseOutline class="text-red-600"/></button>
+            </form>
+          {:else}
+            <button on:click={() => editRow(user)}><EditOutline class="text-green-700 mr-2"/></button>
+            <button on:click={() => {DeleteModal = true; eq = user}}><TrashBinOutline class="text-red-700"/></button>
+          {/if}
+        </TableBodyCell>
+
       </TableBodyRow>
       {/each}
     </TableBody>
@@ -164,10 +289,26 @@
   
 </div>
 
-<Modal title="Delete {userName} from database?" bind:open={DeleteModal} autoclose>
-  <div class="flex gap-5 justify-center">
-      <GradientButton color="green">Confirm</GradientButton>
-      <GradientButton color="green">Cancel</GradientButton>
+<Modal size="xs" bind:open={DeleteModal} outsideclose>
+  <div class="text-center">
+    <h3 class="mb-1 text-lg font-bold text-gray-800 dark:text-gray-400">Delete equipment from database?</h3>
+    <p class="mb-6 text-sm">You are about to delete this entry. This cannot be undone.</p>
+    <p class="text-gray-800">First name: <span style="font-weight: 600;">{eq.first_name}</span></p>
+    <p class="text-gray-800">Last name: <span style="font-weight: 600;">{eq.last_name}</span></p>
+    <p class="text-gray-800">Email: <span style="font-weight: 600;">{eq.email}</span></p>
+    <p class="text-gray-800">Phone: <span style="font-weight: 600;">{eq.phone}</span></p>
+    <p class="text-gray-800">Student number: <span style="font-weight: 600;">{eq.student_number}</span></p>
+    <p class="text-gray-800">Course: <span style="font-weight: 600;">{eq.course}</span></p>
+    <p class="text-gray-800">Department: <span style="font-weight: 600;">{eq.department}</span></p>
+    <p class="text-gray-800">Access level: <span style="font-weight: 600;">{eq.access_level}</span></p>
+    <p class="mb-6 text-gray-800">Description: <span style="font-weight: 600;">{eq.description}</span></p>
+    <form method="POST" action="?/deleteUser" use:enhance={deleteUser}>
+      <input type="hidden" name="id" value={eq.id}/>
+      <div class="flex mb-1 gap-1 justify-center">
+        <Button type="submit" disabled={loading} color="red" class="me-2"><TrashBinOutline class="text-white mr-1"/>Delete</Button>
+        <Button color="alternative" disabled={loading} on:click={() => DeleteModal = false}>Cancel</Button>
+      </div>
+    </form>
   </div>
 </Modal>
 
