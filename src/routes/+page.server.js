89de079f6a82b17de19c0c2e050b0cdc56041525
shankencedric <legara.cedric@gmail.com>
2025-08-23 15:39:48 +0800

    fix: import admin email

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,4 @@
-import { SESSION_COOKIE_NAME } from '$lib/server/constants.js';
+import { SESSION_COOKIE_NAME, ADMIN_EMAIL } from '$lib/server/constants.js';
 import { createSessionByEmail } from '$lib/server/auth.js';
 import { fail, redirect } from "@sveltejs/kit";
 
